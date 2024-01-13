339ac241f7fb66ed0ce177d187dd8dfd66c7ad75
yanbingyuxinyu <92080338+yanbingyuxinyu@users.noreply.github.com>
2024-01-13 22:19:51 +0800

    Update setup_bevm.sh

--- setup_bevm.sh
+++ setup_bevm.sh
@@ -2,13 +2,13 @@
 
 # 询问用户选择节点名称的方式
 read -p "请选择节点名称方式：
-1. 随机节点名称（回车默认） 
+1. ... ... ... 随机节点名称（回车默认） 
 2. 手工输入节点名称 " OPTION
 
 if [ "$OPTION" = "2" ]; then
   read -p "请输入节点名称: " NODE_NAME
 else
-  NODE_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10)
+  NODE_NAME=$(head /dev/urandom | tr -dc ... ... ... A-Za-z0-9 | head -c 10)
   echo "随机生成的节点名称为: $NODE_NAME"
 fi
 
@@ -16,21 +16,24 @@ fi
 echo $NODE_NAME > node_name.txt
 
 # 下载文件
-wget https://github.com/btclayer2/BEVM/releases/download/testnet-v0.1.1/bevm-v0.1.1-ubuntu20.04
+wget ... ... ... https://github.com/btclayer2/BEVM/releases/download/testnet-v0.1.1/bevm-v0.1.1-ubuntu20.04
 
 # 赋予执行权限
 chmod +x bevm-v0.1.1-ubuntu20.04
 
-# ... ... ... 创建 systemd unit 文件
+# 启动节点
+nohup /root/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$NODE_NAME" --pruning=archive &
+
+# 创建 systemd unit 文件
 cat <<EOF > /etc/systemd/system/bevm.service
 [Unit]
-Description=BEVM ... ... ... ... ... ... ... ... ... Node
+Description=BEVM ... ... ... ... ... ... Node
 After=network.target
 
 [Service]
 User=your_username
 WorkingDirectory=/path/to/your/bevm/directory
-ExecStart=/path/to/your/bevm/directory/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$NODE_NAME" ... ... ... ... ... ... ... ... ... --pruning=archive
+ExecStart=/path/to/your/bevm/directory/bevm-v0.1.1-ubuntu20.04 --chain=testnet --name="$NODE_NAME" ... ... ... ... ... ... --pruning=archive
 Restart=always
 StandardOutput=syslog
 StandardError=syslog
@@ -41,5 +44,5 @@ WantedBy=multi-user.target
 EOF
 
 # 启动服务并设置开机自启动
-systemctl start bevm
-systemctl enable bevm
+systemctl start ... bevm
+systemctl ... enable ... bevm
